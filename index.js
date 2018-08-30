7f47dfb5422af19ee3ae58cee5e87db3737b37da
Gabriel Cardona <gabriel@bitcoin.com>
2018-08-30 15:51:09 -0700

    Register, login and whoami.

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 #!/usr/bin/env node
 require("babel-register");
-// let path = require('path');
 let program = require('commander');
 let axios = require('axios');
-// let chalk = require('chalk');
-// let mkdirp = require('mkdirp');
-// let cpFile = require('cp-file');
-// let figlet = require('figlet');
-// let clear = require('clear');
-// let fs = require('fs');
-// let os = require('os');
-// let touch = require("touch");
-// let emoji = require('node-emoji');
-// let repl = require("repl");
-// let ini = require('ini');
-// let Wormhole = require('./lib/Wormhole').default;
-// let clone = require('git-clone');
-// let cmd = require('node-cmd');
+let prompt = require('prompt');
+let tokenStore = require('token-store');
+let tokens = tokenStore({ filename: 'tokenstoretest' });
 
 program
   .version('0.0.3', '-v, --version')
+
+program
+  .command('auth:register')
+  .description(`Register for Bitcoin.com credentials`)
+  .action(async () => {
+    prompt.start();
+    prompt.get(['username', 'password'], async (err, result) => {
+      try {
+        let response = await axios.post(`${process.env.REST_URL}users`, {
+          user: {
+            username: result.username,
+            password: result.password
+          }
+        });
+        tokens.set({
+          server: 'http://localhost:5000',
+          user: JSON.stringify(response.data.user),
+          token: response.data.token
+        });
+      } catch (error) {
+        throw error;
+      }
+    });
+});
 
 program
   .command('auth:2fa')
@@ -37,12 +49,18 @@ program
   .command('auth:login')
   .description(`login with your Bitcoin.com credentials`)
   .action(async () => {
-    try {
-    let response = await axios.post(`${process.env.REST_URL}auth/login`);
-      console.log(response.data)
-    } catch (error) {
-      throw error;
-    }
+    prompt.start();
+    prompt.get(['username', 'password'], async (err, result) => {
+      try {
+        let response = await axios.post(`${process.env.REST_URL}auth`, {
+          username: result.username,
+          password: result.password
+        });
+        console.log(response.data)
+      } catch (error) {
+        throw error;
+      }
+    });
 });
 
 program
@@ -61,11 +79,9 @@ program
   .command('auth:token')
   .description(`outputs current CLI authentication token`)
   .action(async () => {
-    try {
-    let response = await axios.get(`${process.env.REST_URL}auth/token`);
-      console.log(response.data)
-    } catch (error) {
-      throw error;
+    if (tokens.has('http://localhost:5000')) {
+      current = tokens.current('http://localhost:5000');
+      console.log(current.token);
     }
 });
 
@@ -73,48 +89,14 @@ program
   .command('auth:whoami')
   .description(`display the current logged in user`)
   .action(async () => {
-    try {
-      let response = await axios.get(`${process.env.REST_URL}auth/whoami`);
-      console.log(response.data)
-    } catch (error) {
-      throw error;
+    if (tokens.has('http://localhost:5000')) {
+      current = tokens.current('http://localhost:5000');
+      console.log({
+        username: JSON.parse(current.user).username,
+        id: JSON.parse(current.user)._id
+      });
     }
 });
-
-// program
-//   .command('console')
-//   .option('-e, --environment <environment>', 'environment of running BITBOX instance. Ex: production, staging. (Default: development)')
-//   .description('Run a console with Bitcoin Cash RPC commands available')
-//   .action((options) => {
-//     let config;
-//     try {
-//       config = require(process.cwd() + '/wormhole.js').config;
-//     } catch(err) {
-//       console.log(chalk.red('Console command must be run inside a wormholecash project'));
-//       process.exit(1);
-//     }
-//     let replServer = repl.start('> ');
-//     let historyFile = path.join(process.cwd(), '.console_history');
-//     require('repl.history')(replServer, historyFile);
-//
-//     let environment = fetchOption('environment=development', config, options);
-//
-//     replServer.context.Wormhole = new Wormhole(config.networks[environment]);
-//   }
-// );
-//
-// function fetchOption(kv, config, options) {
-//   let parts = kv.split('=');
-//   let key = parts[0];
-//   let defaultVal = parts[1];
-//   if(options && options[key]) {
-//     return options[key];
-//   } else if(config && config.new && config.new[key]) {
-//     return config.new[key];
-//   } else {
-//     return defaultVal;
-//   }
-// }
 
 program
   .parse(process.argv);
